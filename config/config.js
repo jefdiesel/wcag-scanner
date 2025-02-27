953873f9d75ec9fbdff6d050643ae70c89104cfb
jefdiesel <jefdieselnyc@gmail.com>
2025-02-27 13:17:19 -0500

    Remove 5000 page limit from queue form

--- config/config.js
+++ config/config.js
@@ -11,8 +11,8 @@ module.exports = {
   // Scanner configuration
   PLAYWRIGHT_TIMEOUT: 60000, // 60 seconds
   PLAYWRIGHT_ARGS: ['--no-sandbox', '--disable-setuid-sandbox'],
-  DEFAULT_MAX_PAGES: 1000,
-  MAX_CONCURRENT_SCANS: 1, // How many scans can run at once
+  DEFAULT_MAX_PAGES: 1000000,
+  MAX_CONCURRENT_SCANS: 2, // How many scans can run at once
   
   // Report configuration
   REPORT_DIR: './public/reports',
